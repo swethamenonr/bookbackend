48d057af4add25d365730a7db7aba59ec2d1eb05
Swetha Menon <100666112+swethamenonr@users.noreply.github.com>
2022-03-20 09:12:11 +0300

    Update SpringBootBackendPartApplication.java

--- src/main/java/com/abc/in/SpringBootBackendPartApplication.java
+++ src/main/java/com/abc/in/SpringBootBackendPartApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import javax.sql.DataSource;
 import com.abc.in.model.Book;
 import com.abc.in.repository.BookRepository;
 
